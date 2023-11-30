f5c06e868f819c3d09f80c27f3d46a4150410e45
Derix Venegas <derix.dv19@gmail.com>
2023-11-30 11:28:02 -0400

    componente Ejercicio realizado

---
+++ src/Ejercicio/Ejercicio.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Ejercicio.css'
+
+export const Ejercicio = ({ nombreEjercicio }) => {
+  return (
+    <div className='ejercicio'>{nombreEjercicio}</div>
+  )
+}
