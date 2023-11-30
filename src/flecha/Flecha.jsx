c2dacdfe9d7690dc02ed545789c2c8330517e2db
Derix Venegas <derix.dv19@gmail.com>
2023-11-30 11:12:54 -0400

    componente flecha realizado

---
+++ src/flecha/Flecha.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './flecha.css'
+
+export const Flecha = () => {
+  return (
+    <button className="Bflech">
+      <img src="./public/Icons.svg" alt="icons" className="icons" />
+    </button>
+  );
+};
